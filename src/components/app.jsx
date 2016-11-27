5ee5ad46fcb5b66828a98692a64fd5eed3eda363
Janelle Rosario <janelle.rosario@gmail.com>
2016-11-27 17:51:46 -0500

    resolving conflicts

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -332,12 +332,9 @@ class App extends Component {
               updateFormHandler={this.updateFormHandler.bind(this)}
               getWorkPlaces={this.getWorkPlaces.bind(this)}
             />
-<<<<<<< HEAD
             </div>
             <div className="saved-map">
-=======
             <div style={{ width: '100%', height: '46%' }}>
->>>>>>> master
             <SavedMap
               center={location}
               markers={this.state.saved}
@@ -348,11 +345,8 @@ class App extends Component {
             <WorkPlaces
               work={this.state.work}
             />
-<<<<<<< HEAD
               <div className="workplace-map">
-=======
               <div style={{ width: '100%', height: '46%' }}>
->>>>>>> master
                 <WorkPlacesMap
                   center={this.state.workCenter}
                   markers={this.state.work}
